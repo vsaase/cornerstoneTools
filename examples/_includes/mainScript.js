7e963c8530d9d311bec10ddb717126af9c6c3c3b
Victor Saase <vsaase@gmail.com>
2021-04-11 12:29:26 +0200

    start creating example

--- examples/_includes/mainScript.js
+++ examples/_includes/mainScript.js
@@ -11,7 +11,7 @@
 
 const baseUrl =
   window.ENVIRONMENT === 'development'
-    ? 'http://localhost:4000/'
+    ? 'http://127.0.0.1:4000/'
     : 'https://tools.cornerstonejs.org/examples/';
 
 _initCornerstone();
